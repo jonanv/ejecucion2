a94e8f4dfe4be993b27370e12307013d7d41a01f
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-10-05 11:09:24 -0500

    Se hace separacion de fragmentos del layout en las diferentes partes, header, menu, footer, content-wrapper, styles y scripts

---
+++ laborales/views/shared/styles.php
@@ -0,0 +1,6 @@
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="<?php echo SERVERURL ?>views/plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style - Bootstrap -->
+<link rel="stylesheet" href="<?php echo SERVERURL ?>views/plugins/adminlte/css/adminlte.min.css">
