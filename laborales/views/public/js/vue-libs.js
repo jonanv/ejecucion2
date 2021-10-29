e6005f795ed03951b75f95d8b2c75f85d6833bcd
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-10-29 11:04:22 -0500

    Se agregan los dias de vacaciones y dias de semana santa completos a los dias que se van a tener en cuenta que no son laborales para la rama judicial

--- laborales/views/public/js/vue-libs.js
+++ laborales/views/public/js/vue-libs.js
@@ -16,9 +16,13 @@ const NEXT_DAY = {
     NONE: 7
 }
 const EASTER_WEEK_HOLIDAYS = [
-    { day: -3, daysToSum: NEXT_DAY.NONE, celebration: 'Lunes Santo' },
-    { day: -3, daysToSum: NEXT_DAY.NONE, celebration: 'Martes Santo' },
-    { day: -3, daysToSum: NEXT_DAY.NONE, celebration: 'Miercoles Santo' },
+    // Días para la Rama Judicial
+    { day: -7, daysToSum: NEXT_DAY.NONE, celebration: 'Domingo Santo' },
+    { day: -6, daysToSum: NEXT_DAY.NONE, celebration: 'Lunes Santo' },
+    { day: -5, daysToSum: NEXT_DAY.NONE, celebration: 'Martes Santo' },
+    { day: -4, daysToSum: NEXT_DAY.NONE, celebration: 'Miercoles Santo' },
+    { day: -1, daysToSum: NEXT_DAY.NONE, celebration: 'Sabado Santo' },
+    // Días por defecto
     { day: -3, daysToSum: NEXT_DAY.NONE, celebration: 'Jueves Santo' },
     { day: -2, daysToSum: NEXT_DAY.NONE, celebration: 'Viernes Santo' },
     { day: 39, daysToSum: NEXT_DAY.MONDAY, celebration: 'Ascensión del Señor' },
@@ -26,6 +30,7 @@ const EASTER_WEEK_HOLIDAYS = [
     { day: 68, daysToSum: NEXT_DAY.MONDAY, celebration: 'Sagrado Corazón de Jesús' }
 ];
 const HOLIDAYS = [
+    // Días por defecto
     { day: '01-01', daysToSum: NEXT_DAY.NONE, celebration: 'Año Nuevo' },
     { day: '05-01', daysToSum: NEXT_DAY.NONE, celebration: 'Día del Trabajo' },
     { day: '07-20', daysToSum: NEXT_DAY.NONE, celebration: 'Día de la Independencia' },
@@ -39,8 +44,28 @@ const HOLIDAYS = [
     { day: '10-12', daysToSum: NEXT_DAY.MONDAY, celebration: 'Día de la Raza' },
     { day: '11-01', daysToSum: NEXT_DAY.MONDAY, celebration: 'Todos los Santos' },
     { day: '11-11', daysToSum: NEXT_DAY.MONDAY, celebration: 'Independencia de Cartagena' },
+    // Días para la Rama Judicial
     { day: '12-17', daysToSum: NEXT_DAY.NONE, celebration: 'Día de la Rama Judicial' },
     { day: '12-20', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-21', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-22', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-23', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-24', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-26', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-27', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-28', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-29', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-30', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '12-31', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-02', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-03', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-04', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-05', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    // Se agrega nuevamente el 01-06 porque siempre se celebra el lunes siguiente y cuando cae el 06 queda como dia normal
+    { day: '01-06', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-07', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-08', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
+    { day: '01-09', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
     { day: '01-10', daysToSum: NEXT_DAY.NONE, celebration: 'Vacaciones de la Rama Judicial' },
 ];
 const MILLISECONDS_DAY = 86400000;
